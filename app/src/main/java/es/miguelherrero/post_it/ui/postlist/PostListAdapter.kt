cd97ecfd3ef44ac28fe662ab74934f83f5333f99
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2018-08-05 23:24:59 +0200

    Implement missing functions of ViewHolder in Adapter with post data

--- app/src/main/java/es/miguelherrero/post_it/ui/postlist/PostListAdapter.kt
+++ app/src/main/java/es/miguelherrero/post_it/ui/postlist/PostListAdapter.kt
@@ -9,23 +9,32 @@ import es.miguelherrero.post_it.R
 import es.miguelherrero.post_it.domain.model.Post
 import kotlinx.android.synthetic.main.item_post.view.*
 
-class PostListAdapter(): RecyclerView.Adapter<PostListAdapter.PostListAdapterViewHolder>() {
+class PostListAdapter: RecyclerView.Adapter<PostListAdapter.PostListAdapterViewHolder>() {
 
+    // 1 - Store the list data
     lateinit var data: List<Post>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostListAdapterViewHolder {
+        // 2 - Inflate the item view layout and pass it to the viewHolder
         val viewHolder = LayoutInflater.from(parent.context).inflate(R.layout.item_post, parent, false)
         return PostListAdapterViewHolder(viewHolder)
     }
 
     override fun getItemCount(): Int {
+        // 3 - Return how many items are there
         return data.let { it.size }
     }
 
     override fun onBindViewHolder(holder: PostListAdapterViewHolder, position: Int) {
+        // 4 - Fill the viewHolder text views with the data
         data.let { posts ->
             holder.title.text = posts[position].title
         }
+    }
+
+    // 5 - Create an auxiliary function to populate data from other source
+    fun setupPosts(posts: List<Post>) {
+        data = posts
     }
 
     class PostListAdapterViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
