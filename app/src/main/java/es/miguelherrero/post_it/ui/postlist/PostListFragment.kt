8237d967a394f5ebbf715049d31cbb6393a3e679
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2018-08-05 23:25:30 +0200

    Add the recyclerView and its adapter to the Fragment

--- app/src/main/java/es/miguelherrero/post_it/ui/postlist/PostListFragment.kt
+++ app/src/main/java/es/miguelherrero/post_it/ui/postlist/PostListFragment.kt
@@ -6,9 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import es.miguelherrero.post_it.R
+import es.miguelherrero.post_it.domain.model.Post
 
 class PostListFragment : Fragment() {
+
+    private lateinit var mPostList: RecyclerView
+    private lateinit var mPostListAdapter: PostListAdapter
 
     companion object {
         fun newInstance() = PostListFragment()
@@ -18,13 +24,29 @@ class PostListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.post_list_fragment, container, false)
+        val view = inflater.inflate(R.layout.fragment_post_list, container, false)
+
+        // 1 - Find the RecyclerView for posts list
+        mPostList = view.findViewById(R.id.rv_post_list)
+
+        // 2 - Init the adapter
+        mPostListAdapter = PostListAdapter()
+
+        // 3 - Configure de RecyclerView with the adapter
+        val context = activity
+        mPostList.layoutManager = LinearLayoutManager(context)
+        mPostList = view.findViewById(R.id.rv_post_list)
+        mPostList.adapter = mPostListAdapter
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(PostListViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        // 4 - To test, use custom function in adapter to set some initial data
+        mPostListAdapter.setupPosts(listOf(Post("", "", "My first test post", "")))
     }
 
 }
