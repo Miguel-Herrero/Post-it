f4d6f6dda059676c6accff759d1913308f209ca7
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2018-08-06 22:40:58 +0200

    Add repository call from ViewModel to get posts

--- app/src/main/java/es/miguelherrero/post_it/ui/postlist/PostListViewModel.kt
+++ app/src/main/java/es/miguelherrero/post_it/ui/postlist/PostListViewModel.kt
@@ -1,7 +1,12 @@
 package es.miguelherrero.post_it.ui.postlist
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import es.miguelherrero.post_it.data.repository.PostRepository
+import es.miguelherrero.post_it.domain.model.Post
+import javax.inject.Inject
 
-class PostListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class PostListViewModel: ViewModel() {
+    @Inject lateinit var postRepository: PostRepository
+    fun getPosts(): LiveData<List<Post>> = postRepository.getPosts()
 }
