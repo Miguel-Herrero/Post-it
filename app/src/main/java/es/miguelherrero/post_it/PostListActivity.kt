b17478cb8d6d5b7e54a86af5a48c4cecdbe25b75
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2018-08-07 18:37:58 +0200

    Add AndroidInjection (will inject any DI objects referenced by this  activity)

--- app/src/main/java/es/miguelherrero/post_it/PostListActivity.kt
+++ app/src/main/java/es/miguelherrero/post_it/PostListActivity.kt
@@ -2,18 +2,22 @@ package es.miguelherrero.post_it
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.android.AndroidInjection
 import es.miguelherrero.post_it.ui.postlist.PostListFragment
 
 class PostListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidInjection.inject(this)
         setContentView(R.layout.post_list_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, PostListFragment.newInstance())
                     .commitNow()
         }
+
+
     }
 
 }
